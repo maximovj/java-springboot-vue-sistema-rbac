4e591c2ad25e44b73a3e5d11520418dd5fced81a
Víctor J. <maximovj@outlook.com>
2025-12-20 23:01:08 -0600

    * Se elimina la entidad, controlador, repositorio de Oficina

--- backend/src/main/java/com/github/maximovj/rhhub_app/repository/OficinaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.maximovj.rhhub_app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.github.maximovj.rhhub_app.entity.OficinaEntity;
-
-public interface OficinaRepository extends JpaRepository<OficinaEntity, Long> {
-    
-}
